d180e512941e24c269e261b00f13f5a21b58b2f0
ktecheducation <karaulitecheducation@gmail.com>
2024-03-30 11:14:16 +0530

    new commit again

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log(" project ok")
+import mongoose from "mongoose"
+import express  from "express";
+import {dbname} from "./constants.js"
+import dotenv from "dotenv"
+dotenv.config({
+    path:"./.env"
+})
+const app= express();
+;(async ()=>{
+     try{
+        await  mongoose.connect(`${process.env.DB_URL}/${dbname}`)
+        console.log("db connected")
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listenning onthe port number : ${process.env.PORT}`)
+        })
+     }catch(error){
+        console.log("error ---------",error)
+        console.log("db not connected")
+     }
+})()
